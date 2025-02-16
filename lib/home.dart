1c2169b85463530d72a24f269b200eb735f61813
Vainyala17 <samalvainyala174@gmail.com>
2025-02-16 17:56:54 +0530

    initial commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,12 @@
-import 'dart:async';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'CategoryOptions/BankingPage.dart';
+import 'CategoryOptions/CertificatesPage.dart';
+import 'CategoryOptions/EducationPage.dart';
+import 'CategoryOptions/GovernmentPage.dart';
+import 'CategoryOptions/HealthPage.dart';
 import 'app_drawer.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -15,13 +19,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late FirebaseMessaging _messaging;
   final PageController _pageController = PageController();
   final TextEditingController _searchController = TextEditingController();
-  late Timer _timer;
   int _currentIndex = 0;
-  int _currentPage = 0;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final List<Map<String, String>> categories = [
+    {'name': 'Education', 'image': 'assets/images/education.png'},
+    {'name': 'Bank', 'image': 'assets/images/bank.png'},
+    {'name': 'Government', 'image': 'assets/images/government.png'},
+    {'name': 'Healthcare', 'image': 'assets/images/health.png'},
+    {'name': 'Certificates', 'image': 'assets/images/certificate.png'},
+    {'name': 'Healthcare', 'image': 'assets/images/health.png'},
+  ];
 
   final List<String> imageUrls = [
     'https://egov.eletsonline.com/wp-content/uploads/2016/04/MahaLogo-1.png',
@@ -35,23 +45,16 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
-      if (_currentPage < imageUrls.length - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
-      _pageController.animateToPage(_currentPage, duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
-    });
     _setupFCM();
   }
 
   void _setupFCM() async {
-    _messaging = FirebaseMessaging.instance;
-    NotificationSettings settings = await _messaging.requestPermission(alert: true, badge: true, sound: true);
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true, badge: true, sound: true,
+    );
+
     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      String? token = await _messaging.getToken();
-      print("FCM Token: $token");
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         if (message.notification != null) {
           _showNotificationDialog(message.notification!.title, message.notification!.body);
@@ -74,7 +77,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void dispose() {
     _pageController.dispose();
-    _timer.cancel();
     _searchController.dispose();
     super.dispose();
   }
@@ -90,15 +92,18 @@ class _HomePageState extends State<HomePage> {
             enlargeCenterPage: true,
             viewportFraction: 1.0,
             onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                });
+              setState(() => _currentIndex = index);
             },
           ),
           items: imageUrls.map((url) {
             return ClipRRect(
               borderRadius: BorderRadius.circular(10),
-              child: Image.network(url, fit: BoxFit.contain, width: double.infinity),
+              child: Image.network(
+                url,
+                fit: BoxFit.contain,
+                width: double.infinity,
+                errorBuilder: (context, error, stackTrace) => Image.asset('assets/images/placeholder.png', height: 200),
+              ),
             );
           }).toList(),
         ),
@@ -107,19 +112,66 @@ class _HomePageState extends State<HomePage> {
           activeIndex: _currentIndex,
           count: imageUrls.length,
           effect: ExpandingDotsEffect(
-            dotHeight: 8,
-            dotWidth: 8,
-            activeDotColor: Colors.white,
-            dotColor: Colors.grey.shade400,
+            dotHeight: 8, dotWidth: 8, activeDotColor: Colors.white, dotColor: Colors.grey.shade400,
           ),
         ),
       ],
     );
   }
 
+  Widget _buildCategoryGrid() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.9,
+      ),
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+            onTap: () {
+              // Navigate based on category name
+              switch (categories[index]['name']) {
+                case 'Education':
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => EducationPage()));
+                  break;
+                case 'Bank':
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => BankingPage()));
+                  break;
+                case 'Government':
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => GovernmentPage()));
+                  break;
+                case 'Healthcare':
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HealthPage()));
+                  break;
+                case 'Certificates':
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => CertificatesPage()));
+                  break;
+                default:
+                  break;
+              }
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(categories[index]['image']!, height: 130),
+                  SizedBox(height: 8),
+                  Text(categories[index]['name']!, style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final User? currentUser = FirebaseAuth.instance.currentUser;
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -127,26 +179,30 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         leading: IconButton(
           icon: Icon(Icons.menu, color: Colors.white),
-          onPressed:() {
-             _scaffoldKey.currentState?.openDrawer();
-          }
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
         ),
         title: Container(
           decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
           child: TextField(
             controller: _searchController,
-            decoration: InputDecoration(hintText: "Search...", prefixIcon: Icon(Icons.search, color: Colors.black54), border: InputBorder.none, contentPadding: EdgeInsets.symmetric(vertical: 12)),
+            decoration: InputDecoration(
+              hintText: "Search...",
+              prefixIcon: Icon(Icons.search, color: Colors.black54),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(vertical: 12),
+            ),
           ),
         ),
       ),
-      drawer: AppDrawer(favoriteLinks: [],),
-      body: Column(
-        children:
-         [
-          _buildImageSlider(),
-          SizedBox(height: 8),
-          Expanded(child: Center(child: Text("Home Page Content Here"))),
-        ],
+      drawer: AppDrawer(favoriteLinks: []),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildImageSlider(),
+            SizedBox(height: 8),
+            _buildCategoryGrid(),
+          ],
+        ),
       ),
     );
   }
